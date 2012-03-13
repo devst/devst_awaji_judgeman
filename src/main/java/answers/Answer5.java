508916d9b54a0d9c87e57f0f2607d8b48314cac0
fishiiiii <fishiiiii@gmail.com>
2012-03-14 02:21:07 +0900

    Update src/main/java/answers/Answer5.java

--- src/main/java/answers/Answer5.java
+++ src/main/java/answers/Answer5.java
@@ -1,13 +1,14 @@
-package answers;
+package models.answers;
 
 /**
  * ローマ数字変換
  * 
- * ローマ数字を文字列として渡し、アラビア数字へ変換して整数を返す。
+ * ローマ数字を文字列として受け取り、int型に変換して返す。
  * 
  */
 
 public interface Answer5 {
 
-	int arabian(String... roman);
+	int toInt(String roman);
+
 }
