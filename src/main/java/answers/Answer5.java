1c7493e21d26d23b5f03090790c39e2378454fc4
Makoto Morita <mmakmo@gmail.com>
2012-03-13 12:29:37 +0900

    add answers #5~7

---
+++ src/main/java/answers/Answer5.java
@@ -0,0 +1,13 @@
+package answers;
+
+/**
+ * ローマ数字変換
+ * 
+ * ローマ数字を文字列として渡し、アラビア数字へ変換して整数を返す。
+ * 
+ */
+
+public interface Answer5 {
+
+	int arabian(String... roman);
+}
---
+++ src/main/java/answers/Answer7.java
@@ -0,0 +1,14 @@
+package answers;
+
+/**
+ * 鶴亀算
+ * 
+ * 鶴と亀の全個体数と、両者の足の本数をそれぞれ正の整数で渡し、鶴と亀それぞれの個体数を文字列として返す。
+ * 鶴の足は2脚、亀は4脚とし、答えは「鶴x羽、亀y匹」(x, yはそれぞれの個体数)とする。
+ * 
+ */
+
+public interface Answer7 {
+
+	String tsurukame(int x, int y);
+}
