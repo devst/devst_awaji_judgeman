bf54b63c5bf8ff462b492f2e7ed5968a42cf679f
irof <@irof>
2012-03-21 00:12:22 +0900

    FizzBuzzのJudgeパターン追加

--- src/main/java/judges/Judge3.java
+++ src/main/java/judges/Judge3.java
@@ -15,19 +15,34 @@ public class Judge3 {
 	@Test
 	public void 三の倍数() throws Exception {
 		assertThat(sut.fizzBuzz(3), is("Fizz"));
-		assertThat(sut.fizzBuzz(15), is("FizzBuzz"));
+		assertThat(sut.fizzBuzz(18), is("Fizz"));
 	}
 
+	@Test
 	public void 五の倍数() throws Exception {
 		assertThat(sut.fizzBuzz(5), is("Buzz"));
+		assertThat(sut.fizzBuzz(35), is("Buzz"));
 	}
 
+	@Test
 	public void 三と五の倍数() throws Exception {
 		assertThat(sut.fizzBuzz(15), is("FizzBuzz"));
+		assertThat(sut.fizzBuzz(150), is("FizzBuzz"));
 	}
 
+	@Test
 	public void その他() throws Exception {
 		assertThat(sut.fizzBuzz(4), is("4"));
 		assertThat(sut.fizzBuzz(7), is("7"));
+	}
+
+	@Test(expected=RuntimeException.class)
+	public void ゼロの場合() throws Exception {
+		sut.fizzBuzz(0);
+	}
+
+	@Test(expected=RuntimeException.class)
+	public void マイナスの場合() throws Exception {
+		sut.fizzBuzz(-3);
 	}
 }
