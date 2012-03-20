4458f69807db3800a17144f64847d423dd6edbe8
Makoto Morita <mmakmo@gmail.com>
2012-03-20 20:38:15 +0900

    問題3(FizzBuzz)のテストを追加
    1. テストととして Judge3.java を追加
    2. .DS_Store を .gitignore に追記

--- src/main/java/judges/Judge3.java
+++ src/main/java/judges/Judge3.java
@@ -15,5 +15,19 @@ public class Judge3 {
 	@Test
 	public void 三の倍数() throws Exception {
 		assertThat(sut.fizzBuzz(3), is("Fizz"));
+		assertThat(sut.fizzBuzz(15), is("FizzBuzz"));
+	}
+
+	public void 五の倍数() throws Exception {
+		assertThat(sut.fizzBuzz(5), is("Buzz"));
+	}
+
+	public void 三と五の倍数() throws Exception {
+		assertThat(sut.fizzBuzz(15), is("FizzBuzz"));
+	}
+
+	public void その他() throws Exception {
+		assertThat(sut.fizzBuzz(4), is("4"));
+		assertThat(sut.fizzBuzz(7), is("7"));
 	}
 }
